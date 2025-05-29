b0c4656b8083b3979f2d6f88caa29aaa79fe9371
hernandp <hernan.di.pietro@gmail.com>
2025-05-29 19:50:40 -0300

    deployment contract values

--- ethereum/ts-scripts/relayer/helpers/deployments.ts
+++ ethereum/ts-scripts/relayer/helpers/deployments.ts
@@ -360,7 +360,12 @@ export async function buildOverrides(
   } else if (chain.chainId === 48) { 
     overrides.type = 2;
     overrides.maxPriorityFeePerGas = ethers.utils.parseUnits("2", "gwei");
+  } else if (chain.chainId === 10007) {
+    overrides.type = 2;
+    overrides.maxPriorityFeePerGas = ethers.utils.parseUnits("25", "gwei");
+    overrides.maxFeePerGas = ethers.utils.parseUnits("30", "gwei");
   }
+
   return overrides;
 }
 
