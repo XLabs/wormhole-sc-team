073d3dcb0ddde5d4eb6c9d19ec1c363448466aa1
Sebastián Claudio Nale <sebinale@gmail.com>
2026-03-03 19:52:53 -0300

    client/js: fixes Pythnet submit command

--- clients/js/src/solana.ts
+++ clients/js/src/solana.ts
@@ -67,17 +67,17 @@ export async function execute_solana(
   }
 
   const nftContract = contracts.nftBridge.get(network, chain);
-  if (!nftContract && chain !== "Fogo") {
+  if (!nftContract && chain !== "Fogo" && chain !== "Pythnet") {
     throw new Error(`NFT bridge address not defined for ${chain} ${network}`);
   }
 
   const tbContract = contracts.tokenBridge.get(network, chain);
-  if (!tbContract) {
+  if (!tbContract && chain !== "Pythnet") {
     throw new Error(`Token bridge address not defined for ${chain} ${network}`);
   }
 
   const bridgeId = new web3s.PublicKey(coreContract);
-  const tokenBridgeId = new web3s.PublicKey(tbContract);
+  const tokenBridgeId = tbContract !== undefined ? new web3s.PublicKey(tbContract) : undefined;
   const nftBridgeId = nftContract !== undefined ? new web3s.PublicKey(nftContract) : undefined;
 
   let ix: web3s.TransactionInstruction;
